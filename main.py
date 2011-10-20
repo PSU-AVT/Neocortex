e6fbc7b8b7fe4ef09b96b0f1aaed720d9fb30d71
Gregory Haynes <greg@greghaynes.net>
2011-10-19 20:46:12 -0700

    debug commands from base station

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import asyncore, socket
 import afproto
+import serial
+import struct
 
 class Neocortex(asyncore.dispatcher):
-	def __init__(self, host, port):
+	def __init__(self, host, port, controller):
 		asyncore.dispatcher.__init__(self)
 		self.create_socket(socket.AF_INET, socket.SOCK_DGRAM)
 		self.set_reuse_addr()
 		self.bind((host, port)) 
+		self.controller = controller
 
 	def handle_read(self):
 		data, addr = self.recvfrom(2048)
-		print str(addr)+" >> "+data 
+		cmd_id, arg = struct.unpack('BB', data)
+		print cmd_id, arg
+		self.controller.write(afproto.serialize_payload(data))
 
 	def writable(self):
 		return False
@@ -19,6 +24,7 @@ class Neocortex(asyncore.dispatcher):
 		pass
 
 if __name__=='__main__':
-	server = Neocortex('', 8080)
+	controller = serial.Serial('/dev/ttyACM0', 38400)
+	server = Neocortex('', 8080, controller)
 	asyncore.loop()
 
