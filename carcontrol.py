6e3e53fcb5a76750bf2c3c5f6b1bcae3f521e2a1
Gregory Haynes <greg@greghaynes.net>
2011-11-01 21:10:55 -0700

    Added response handlers to carcontrol

--- carcontrol.py
+++ carcontrol.py
@@ -1,9 +1,31 @@
 import afprotowatcher
+import logging
 
 class CarControl(afprotowatcher.SerialAfprotoWatcher):
 	def __init__(self, path):
 		afprotowatcher.SerialAfprotoWatcher.__init__(self, path)
+		self.response_handlers = {}
+		self.global_response_handlers = []
 
 	def handle_msg(self, msg):
-		print 'Got %s' % msg
+		logging.debug('Got %d command from car' % ord(msg[0]))
+
+		for handler in self.global_response_handlers:
+			handler(msg)
+
+		try:
+			handlers = self.response_handlers[ord(msg[0])]
+			for handler in handlers:
+				handler(msg)
+		except KeyError:
+			pass
+
+	def add_response_handler(self, response_id, handler):
+		try:
+			self.response_handlers[response_id].append(handler)
+		except KeyError:
+			self.response_handlers[response_id] = [handler]
+
+	def add_global_response_handler(self, handler):
+		self.global_response_handlers.append(handler)
 
--- main.py
+++ main.py
@@ -3,8 +3,13 @@ import evloop
 import controlgw
 import carcontrol
 
+def print_msg(msg):
+	print msg
+
 if __name__=='__main__':
 	controller = carcontrol.CarControl('/dev/ttyACM0')
+	controller.add_global_response_handler(print_msg)
+
 	server = controlgw.ControlGw('', 8080, controller)
 	evloop.EventDispatcher().loop_forever()
 
